b57725f384ca3d019cdcb94a441f94b7dfcbde00
Vikram Ankathi <vikramankathi@gmail.com>
2020-04-18 13:24:55 +0530

    First method

---
+++ Rock_Paper_Scissors_Game.py
@@ -0,0 +1,13 @@
+import random
+
+a = [0, 2, 5]
+
+
+def input():
+    print("Enter the input in below format")
+    print("/'0/' for /'Rock/'")
+    print("/'2/' for /'Scissors/'")
+    print("/'3/' for /'Paper/'")
+
+
+input()
